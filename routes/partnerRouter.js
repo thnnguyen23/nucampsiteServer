80c7e66bca123af5a857d9fb6a001f31fe50d794
Thanh Nguyen <thanhnnguyen28@gmail.com>
2023-05-12 22:44:19 -0400

    Nucampsite Server Commit

---
+++ routes/partnerRouter.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const partnerRouter = express.Router();
+const Partner = require('../models/partner')
+
+partnerRouter.route('/')
+.get((req, res, next) => {
+    Partner.find()
+    .then(partners => res.status(200).json(partners))
+    .catch(err => next(err))
+})
+.post((req, res, next) => {
+    Partner.create(req.body)
+    .then(partners => res.status(200).json(partners))
+    .catch(err => next(err))
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners');
+})
+.delete((req, res, next) => {
+    Partner.deleteMany()
+    .then(partners => res.status(200).json(partners))
+    .catch(err => next(err))
+});
+
+partnerRouter.route('/:partnerId')
+
+.get((req, res, next) => {
+    Partner.findById(eq.params.partnerId)
+    .then(partner => res.status(200).json(partner))
+    .catch(err => next(err))
+})
+.post((req, res) => {
+    redirect.end('POST operation not su pport on /partners/:partnerId')
+})
+.put((req, res, next) => {
+    Partner.findByIdAndUpdate(req.params.partnerId, req.body, { new: true })
+    .then(partner => res.status(200).json(partner))
+    .catch(err => next(err))
+})
+.delete((req, res, next) => {
+    Partner.findByIdAndDelete(req.params.partnerId)
+    .then(partner => res.status(200).json(partner))
+    .catch(err => next(err))
+})
+
+module.exports = partnerRouter;
